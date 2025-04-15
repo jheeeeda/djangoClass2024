3291afc7dbc7f460473fd0865268936f4fdb5395
jheeeeda <jheeeeda@naver.com>
2025-04-15 18:21:04 +0900

    포스트에 파일 올리기

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('<int:pk>/', views.PostDetail.as_view()),
     path('',views.PostList.as_view()),
     # path('<int:pk>/', views.single_post_page),
     # path('', views.index),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
